bfc332c4ca303d8105158a4af962c92393d1fb09
VanLoc <hovanloc.lv@gmail.com>
2024-10-31 05:48:33 +0700

    version12

--- js/products.js
+++ js/products.js
@@ -135,6 +135,161 @@ const products = [
         ],
         description: "Xe Máy Điện E-Scooter YADEA X-Bull (Ắc Quy TTFAR)"
     },
+    {
+        id: 9,
+        name: "E-BIKE DK Tron Z",
+        brand: "DK Bike",
+        origin: "Việt Nam",
+        power: "350W",
+        maxDistance: "50-60km/lần sạc",
+        price: 10490000,
+        colors: ["Cam", "Đen", "Xanh"],
+        images: [
+            "../img/xedapdien7.jpg",
+            "../img/xedapdien8.jpg",
+            "../img/xedapdienModal13.jpg",
+            "../img/xedapdienModal14.jpg",
+            "../img/xedapdienModal15.jpg",
+        ],
+        description: "Xe Đạp Điện E-BIKE DK Tron Z – Bánh 18 Inches"
+    },
+    {
+        id: 10,
+        name: "DK Samurai",
+        brand: "DK Bike",
+        origin: "Việt Nam",
+        power: "450W",
+        maxDistance: "50km/lần sạc",
+        price: 15490000,
+        colors: [, "Đen", "Xanh"],
+        images: [
+            "../img/DK1.jpg",
+            "../img/DK2.jpg",
+        ],
+        description: "Xe Đạp Điện DK Samurai – Bánh 16 Inches"
+    },
+    {
+        id: 11,
+        name: "DK Sparta",
+        brand: "DK Bike",
+        origin: "Việt Nam",
+        power: "600W",
+        maxDistance: "50-60 km/lần sạc",
+        price: 14490000,
+        colors: ["Cam", "Trắng"],
+        images: [
+            "../img/DK3.jpg",
+            "../img/DK4.jpg",
+        ],
+        description: "Xe Đạp Điện DK Sparta – Bánh 16 Inches"
+    },
+    {
+        id: 12,
+        name: "E-Bike BOMELLI B16",
+        brand: "Hitasa",
+        origin: "Việt Nam",
+        power: "230W",
+        maxDistance: "40-50km/lần sạc",
+        price: 9790000,
+        colors: ["Cam", "Đỏ"],
+        images: [
+            "../img/xedapdien5.jpg",
+            "../img/xedapdien6.jpg",
+        ],
+        description: "Xe Đạp Điện E-Bike BOMELLI B16"
+    },
+    {
+        id: 13,
+        name: "E-Bike HITASA 18IM",
+        brand: "Hitasa",
+        origin: "Việt Nam",
+        power: "250W",
+        maxDistance: "40-50km/lần sạc",
+        price: 9590000,
+        colors: ["Cam", "Vàng"],
+        images: [
+            "../img/HTS1.jpg",
+            "../img/HTS2.jpg",
+        ],
+        description: "Xe Đạp Điện E-Bike HITASA 18IM"
+    },
+    {
+        id: 14,
+        name: "E-BIKE HITASA S168",
+        brand: "Hitasa",
+        origin: "Việt Nam",
+        power: "230W",
+        maxDistance: "40-50km/lần sạc",
+        price: 11190000,
+        colors: ["Bạc", "Hồng"],
+        images: [
+            "../img/HTS3.jpg",
+            "../img/HTS4.jpg",
+        ],
+        description: "Xe Đạp Điện E-BIKE HITASA S168"
+    },
+    {
+        id: 15,
+        name: "HITASA N18",
+        brand: "Hitasa",
+        origin: "Việt Nam",
+        power: "250W",
+        maxDistance: "40-50km/lần sạc",
+        price: 9590000,
+        colors: ["Xanh"],
+        images: [
+            "../img/xedapdien15.jpg",
+            "../img/xedapdien16.jpg",
+        ],
+        description: "Xe đạp điện HITASA N18"
+    },
+    {
+        id: 16,
+        name: "E-Bike JVC G10 Premium",
+        brand: "JVC",
+        origin: "Việt Nam, Trung Quốc",
+        power: "250W",
+        maxDistance: "40-50km/lần sạc",
+        price: 11890000,
+        colors: ["Đỏ đen", "Đen"],
+        images: [
+            "../img/xedapdien11.jpg",
+            "../img/xedapdien12.jpg",
+        ],
+        description: "Xe Đạp Điện E-Bike JVC G10 Premium- Bánh 16 Inches"
+    },
+    {
+        id: 17,
+        name: "E-Scooter JVC G9S NFC",
+        brand: "JVC",
+        origin: "Việt Nam, Trung Quốc",
+        power: "500W",
+        maxDistance: "60-70km/lần sạc",
+        price: 14490000,
+        colors: ["Trắng", "Đỏ"],
+        images: [
+            "../img/JVC1.jpg",
+            "../img/JVC2.jpg",
+        ],
+        description: "Xe Máy Điện E-Scooter JVC G9S NFC"
+    },
+    {
+        id: 18,
+        name: "E-Scooter JVC Xmen Neo",
+        brand: "JVC",
+        origin: "Việt Nam, Trung Quốc",
+        power: "500W",
+        maxDistance: "60-70km/lần sạc",
+        price: 15690000,
+        colors: ["Đen", "Xanh"],
+        images: [
+            "../img/JVC3.jpg",
+            "../img/JVC4.jpg",
+        ],
+        description: "Xe Máy Điện E-Scooter JVC Xmen Neo New"
+    },
+
+
 
     // Thêm các sản phẩm khác vào đây
 ];
